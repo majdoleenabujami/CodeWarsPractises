7a1496c5194fc62c741dc21d30ad74cc019799da
majdoleenfayezjami@gmail.com <majdoleenfayezjami@gmail.com>
2023-03-02 12:57:43 +0200

    Code Warse

---
+++ SumPostineNumber.js
@@ -0,0 +1,38 @@
+/*
+//Question 1
+//طريقة 1
+function oddOrEven(number){
+ return (number%2===0)? "Even":"Odd";
+}
+
+console.log(oddOrEven(3))
+
+//طريقة 2
+
+function odd_Even(number){
+ if(number%2===0){
+   return "Even"}
+   else{
+   "Odd"};
+}
+
+console.log(odd_Even(4))
+
+
+
+//Question 2
+function positiveSum(arr) {
+  let sum = 0;
+  
+  for(let i=0;i<arr.length;i++){
+    if(arr[i]>0){
+    sum += arr[i];
+  }}
+  return sum;
+}
+
+console.log(positiveSum([2,3,3,-2]));
+*/
+
+
+
